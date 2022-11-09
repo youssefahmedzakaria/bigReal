ed389f32bbf51b75a66be311afaba222a8a5c335
youssef ahmed <youssefahmed052@gmail.com>
2022-11-09 12:13:19 +0200

    operation +&- edited

--- BigReal.cpp
+++ BigReal.cpp
@@ -22,6 +22,7 @@ BigReal::BigReal(double realNumber) {
         decimalPoint = decimalPlace;
     }
 }
+
 //constructor edited
 BigReal::BigReal(string RealNumber) {
     regex validInput("[+-]?([0-9]*[.])?[0-9]+");
@@ -41,6 +42,13 @@ BigReal::BigReal(string RealNumber) {
     }
 }
 
+int sign(string s) {
+    if (s[0] == '-') {
+        return -1;
+    } else
+        return 1;
+}
+
 BigReal::BigReal(BigDecimalInt& bigInteger){
     rNum = bigInteger.getNumber();
 }
@@ -48,18 +56,21 @@ BigReal::BigReal(BigDecimalInt& bigInteger){
 string BigReal::get_integer() {
     return rNum;
 }
+
 // Copy constructor
 BigReal::BigReal(const BigReal& other) {
     rNum = other.rNum;
     decimalPoint = other.decimalPoint;
 //    cout<<"Copied"<<endl;
 }
+
 //move constructor
 BigReal::BigReal(BigReal &&other) {
     rNum =std::move(other.rNum);
     decimalPoint = std::move(other.decimalPoint);
 //    cout<<"moved"<<endl;
 }
+
 //assignment operator
 BigReal &BigReal::operator=(const BigReal &other) {
     rNum = other.rNum;
@@ -67,6 +78,7 @@ BigReal &BigReal::operator=(const BigReal &other) {
 //    cout<<"Copied from = operator"<<endl;
     return *this;
 }
+
 //Move assignment
 BigReal &BigReal::operator=(BigReal &&other) {
     rNum = std::move(other.rNum);
@@ -74,20 +86,28 @@ BigReal &BigReal::operator=(BigReal &&other) {
 //    cout<<"Moved from = operator"<<endl;
     return *this;
 }
+
 //plus operator
 BigReal BigReal::operator+(BigReal &other) {
     string s1 = rNum;
     string s2 = other.rNum;
+//    to compare the signs of the numbers while adding
+    char sign1 = s1[0];
+    char sign2 = s2[0];
+//    to remove the decimal point from the numbers to do the addition
     s1.erase(remove(s1.begin(), s1.end(), '.'), s1.end());
     s2.erase(remove(s2.begin(), s2.end(), '.'), s2.end());
+//    to remove the sign from the numbers to do the addition
+    s1.erase(remove(s1.begin(), s1.end(), '-'), s1.end());
+    s2.erase(remove(s2.begin(), s2.end(), '-'), s2.end());
+
+//adding zeros to the numbers to make them equal in length from both sides
     if (decimalPoint < other.decimalPoint) {
         int diff = other.decimalPoint - decimalPoint;
         s1.insert(0, diff, '0');
-
     } else if (decimalPoint > other.decimalPoint) {
         int diff = decimalPoint - other.decimalPoint;
         s2.insert(0, diff, '0');
-
     }
     long long n1 = s2.size() - s1.size();
     long long n2 = s1.size() - s2.size();
@@ -101,29 +121,68 @@ BigReal BigReal::operator+(BigReal &other) {
             s2 += "0";
         }
     }
+//here's the number after making them equal in length
+    cout << "the first number = " << s1 << endl;
+    cout << "the second number = " << s2 << endl<<endl;
+    cout<<"result = ";
+//    to do the addition
     BigDecimalInt number1(s1);
     BigDecimalInt number2(s2);
-    BigDecimalInt number3 = number1 + number2;
-    string s3 = number3.getNumber();
-    int pos = max(decimalPoint, other.decimalPoint);
-    s3.insert(pos, 1, '.');
-    BigReal res(s3);
-    return res;
+    if (sign1 == '-' && sign2 == '-') {
+        BigDecimalInt number3 = number1 + number2;
+        string s3 = number3.getNumber();
+        s3.insert(0, 1, '-');
+        int pos = max(decimalPoint, other.decimalPoint);
+        s3.insert(pos+1, 1, '.');
+        BigReal result(s3);
+        return result;
+    } else if (sign1 == '-' && sign2 != '-') {
+        BigDecimalInt number3 = number2 - number1;
+        string s3 = number3.getNumber();
+        s3.insert(0, 1, '-');
+        int pos = max(decimalPoint, other.decimalPoint);
+        s3.insert(pos+1, 1, '.');
+        BigReal result(s3);
+        return result;
+    } else if (sign1 != '-' && sign2 == '-') {
+        BigDecimalInt number3 = number1 - number2;
+        string s3 = number3.getNumber();
+        s3.insert(0, 1, '-');
+        int pos = max(decimalPoint, other.decimalPoint);
+        s3.insert(pos+1, 1, '.');
+        BigReal result(s3);
+        return result;
+    } else {
+        BigDecimalInt number3 = number1 + number2;
+        string s3 = number3.getNumber();
+        int pos = max(decimalPoint, other.decimalPoint);
+        s3.insert(pos, 1, '.');
+        BigReal result(s3);
+        return result;
+    }
 }
 
 BigReal BigReal::operator-(BigReal &other) {
     string s1 = rNum;
     string s2 = other.rNum;
+
+//    to compare the signs of the numbers while adding
+    char sign1 = s1[0];
+    char sign2 = s2[0];
+//    to remove the decimal point from the numbers to do the addition
     s1.erase(remove(s1.begin(), s1.end(), '.'), s1.end());
     s2.erase(remove(s2.begin(), s2.end(), '.'), s2.end());
+//    to remove the sign from the numbers to do the addition
+    s1.erase(remove(s1.begin(), s1.end(), '-'), s1.end());
+    s2.erase(remove(s2.begin(), s2.end(), '-'), s2.end());
+
+//adding zeros to the numbers to make them equal in length from both sides
     if (decimalPoint < other.decimalPoint) {
         int diff = other.decimalPoint - decimalPoint;
         s1.insert(0, diff, '0');
-
     } else if (decimalPoint > other.decimalPoint) {
         int diff = decimalPoint - other.decimalPoint;
         s2.insert(0, diff, '0');
-
     }
     long long n1 = s2.size() - s1.size();
     long long n2 = s1.size() - s2.size();
@@ -137,12 +196,48 @@ BigReal BigReal::operator-(BigReal &other) {
             s2 += "0";
         }
     }
+//here's the number after making them equal in length
+    cout << "the first number = " << s1 << endl;
+    cout << "the second number = " << s2 << endl << endl;
+    cout << "result = ";
     BigDecimalInt number1(s1);
     BigDecimalInt number2(s2);
-    BigDecimalInt number3 = number1 - number2;
-    string s3 = number3.getNumber();
-    int pos = max(decimalPoint, other.decimalPoint);
-    s3.insert(pos, 1, '.');
-    BigReal res(s3);
-    return res;
+//    to do the subtraction
+    if (sign1 != '-' && sign2 == '-') {
+        BigDecimalInt number2(s2);
+        BigDecimalInt number3 = number1 - number2;
+        string s3 = number3.getNumber();
+        int pos = max(decimalPoint, other.decimalPoint);
+        s3.insert(pos, 1, '.');
+        BigReal res(s3);
+        return res;
+    } else if (sign1 == '-' && sign2 != '-') {
+        BigDecimalInt number1(s1);
+        BigDecimalInt number3 = number2 - number1;
+        string s3 = number3.getNumber();
+        s3.insert(0, 1, '-');
+        int pos = max(decimalPoint, other.decimalPoint);
+        s3.insert(pos + 1, 1, '.');
+        BigReal res(s3);
+        return res;
+    } else if (sign1 == '-' && sign2 == '-') {
+        BigDecimalInt number1(s1);
+        BigDecimalInt number2(s2);
+        BigDecimalInt number3 = number1 - number2;
+        string s3 = number3.getNumber();
+        s3.insert(0, 1, '-');
+        int pos = max(decimalPoint, other.decimalPoint);
+        s3.insert(pos + 1, 1, '.');
+        BigReal res(s3);
+        return res;
+    } else {
+        BigDecimalInt number3 = number1 - number2;
+        string s3 = number3.getNumber();
+        int pos = max(decimalPoint, other.decimalPoint);
+        s3.insert(pos, 1, '.');
+        BigReal res(s3);
+        return res;
+    }
 }
+
+
